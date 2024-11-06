0fbd7d1df133bb787332e0650b9e71ef9e1fddcb
Gigi Beridze <beridzegigi19@gmail.comecho>
2024-11-06 16:44:28 +0400

    Main Panel

--- src/Components/Panel.jsx
+++ src/Components/Panel.jsx
@@ -5,6 +5,22 @@ export default function Panel() {
     <div className='panel' >
 
       <div className="panelframe">
+        <img width={250} src="LOGO.png" alt="" />
+        
+
+        <div className="services">
+        ONLINEBANDENKOPEN
+          <a href=""><img src="LEKKEBANDPECHBIJSTAND.png" alt="" /></a>
+          <a href=""><img src="ZOMERWINTERWISSEL.png" alt="" /></a>
+          <a href=""><img src="ONLINEBANDENKOPEN.png" alt="" /></a>
+          <a href=""><img src="GEENBRANDSTOF.png" alt="" /></a>
+          <a href=""><img src="BATTERIJLEEG.png" alt="" /></a>
+          <a href=""><img src="SLOTBOUTVERWIJDEREN.png" alt="" /></a>
+          <a href=""><img src="TAKELEN.png" alt="" /></a>
+          <a href=""><img src="B2BBANDENWISSEL.png" alt="" /></a>
+
+
+        </div>
 
 
 
